3d963ec56ae774f682e3d764251e7ca8d25246b7
afagusta <afagusta@gmail.com>
2026-06-22 21:20:37 +0700

    feat: design database schema for user watchlist and reviews

---
+++ database/seeders/RoleAndUserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User; //
+use Spatie\Permission\Models\Role; 
+use Illuminate\Support\Facades\Hash; 
+
+class RoleAndUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 1. Buat Role Admin dan User
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);[cite: 6]
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+
+        // 2. Buat Akun Admin CineList
+        $admin = User::firstOrCreate([
+            'email' => 'admin@cinelist.com',
+            'name' => 'Admin CineList',
+            'password' => Hash::make('password'),
+        ]);[cite: 6]
+        $admin->assignRole($adminRole);[cite: 6]
+
+        // 3. Buat Akun User Biasa
+        $user = User::firstOrCreate([
+            'email' => 'user@cinelist.com',
+            'name' => 'Pengguna Biasa',
+            'password' => Hash::make('password'),
+        ]);[cite: 6]
+        $user->assignRole($userRole);[cite: 6]
+    }
+}
